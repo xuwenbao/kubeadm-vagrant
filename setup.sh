ce7bb4444391473cb15676ea3276ad07abaa717b
Wenbao Xu <mail@xuwenbao.me>
2017-12-15 11:09:11 +0800

    取消替换dns地址

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ CGROUP_DRIVER=$(sudo docker info | grep "Cgroup Driver" | awk '{print $3}')
 
 sed -i "s|KUBELET_KUBECONFIG_ARGS=|KUBELET_KUBECONFIG_ARGS=--cgroup-driver=$CGROUP_DRIVER --pod-infra-container-image=xuwenbao/pause-amd64:3.0 |g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
-sed -i 's/10.96.0.10/10.3.3.10/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# sed -i 's/10.96.0.10/10.3.3.10/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 systemctl daemon-reload
 
